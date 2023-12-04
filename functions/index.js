166d692e135bc48bdbe92fa352346cd4d78f2c01
rgarro <rgarro@gmail.com>
2023-12-03 21:18:38 -0600

    we all are going to die by our arrogance ...

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,19 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+//const {onRequest} = require("firebase-functions/v2/https");
+//const logger = require("firebase-functions/logger");
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+const {logger} = require("firebase-functions");
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+
+// The Firebase Admin SDK to access Firestore.
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+
+initializeApp();
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
